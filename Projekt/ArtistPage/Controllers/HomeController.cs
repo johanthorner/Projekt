a898a44ad235433b4a175b2cf5d5caab855ac4bf
johanthorner <johan@thorner.se>
2017-01-27 10:21:40 +0100

    flyttat

---
+++ Projekt/ArtistPage/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtistPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projekt/ArtistPortfolio/Scripts/CustomScripts/pageContentScript.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    $('.autoplay').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+    start();
+});
+
+$('#brand').on('click', function () {
+    start();
+});
+
+$('#portfolio').on('click', function () {
+    portfolio();
+});
+
+$('#contact').on('click', function () {
+    contact();
+});
+$('#cv').on('click', function () {
+    cv();
+});
+
+function start() {
+
+    $("#pageContent").html("<div><h1>Start</h1></div>");
+}
+
+function portfolio() {
+
+    $("#pageContent").html("<div><h1>Portfolio</h1></div>");
+}
+
+function contact() {
+
+    $("#pageContent").html("<div><h1>Contact</h1></div>");
+}
+
+function cv() {
+
+    $("#pageContent").html("<div><h1>CV</h1></div>");
+}
+
